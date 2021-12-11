302a4f3b9e5d06c0695e0dbeb600fa502a5aa00a
Luu Chan Thinh <chanthinhluu@gmail.com>
2021-12-11 16:56:10 +0700

    Xu ly Search Pagination

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -6,20 +6,43 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { actFetchArticlesAsync } from "../store/post/actions";
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { useState } from "react";
 
 function SearchPage() {
   const location = useLocation()
   const dispatch = useDispatch()
   const queryStr = getQueryStr('q', location)
 
-  const { list: posts, currentPage, total } = useSelector(state => state.Post.articlePaging)
+  const {
+    list: posts,
+    currentPage,
+    totalPages,
+    total
+  } = useSelector(state => state.Post.articlePaging)
+  const [loading, setLoading] = useState(false)
+  const hasMorePost = currentPage < totalPages
 
   useEffect(() => {
     dispatch(actFetchArticlesAsync({
       search: queryStr,
-      perPage: 2
+      // perPage: 2
     }))
   }, [queryStr, dispatch])
+
+  function handleLoadMore() {
+
+    if (loading) {
+      return
+    }
+
+    setLoading(true)
+    dispatch(actFetchArticlesAsync({
+      currentPage: currentPage + 1,
+      search: queryStr
+    })).then(() => {
+      setLoading(false)
+    })
+  }
 
   return (
     <div className="articles-list section">
@@ -43,9 +66,16 @@ function SearchPage() {
           }
         </div>
 
-        <div className="text-center">
+        {/* <div className="text-center">
           <Button type="primary" size="large">Tải thêm</Button>
-        </div>
+        </div> */}
+        {
+          hasMorePost && (
+            <div className="text-center">
+              <Button onClick={handleLoadMore} type="primary" size="large" loading={loading}>Tải thêm</Button>
+            </div>
+          )
+        }
       </div>
     </div>
 
